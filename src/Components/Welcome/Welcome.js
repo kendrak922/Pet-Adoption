48469dfeacc2fd35c79f34e96b14b7aa83e5a28a
Kendra Kyndberg <kendrak922@gmail.com>
2020-06-20 18:39:47 -0500

    change picture

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -1,17 +1,22 @@
 import './welcome.css';
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Mac from './Mac.JPG';
 
 
 
-class Welcome extends Component{
-  render(){
-   return (
-<div className="container"> 
-    <img src={Mac} className ="tabby"alt="tabbycat"/>
-    <p className="descritption">Welcome to Clyde's Pet Rescue! Let us introduce you to your new best friend. We believe life is better spent with pets. </p>
-</div>
-);
-}
+class Welcome extends Component {
+    render() {
+        return (
+            <div className="container">
+                <img src={Mac} className="tabby" alt="tabbycat" />
+                <div className="right">
+                    <h1>Clyde's Pet Rescue</h1>
+                    <p>Welcome to Clyde's Pet Rescue! Let us introduce you to your new best friend. We believe life is better spent with pets. </p>
+                    <h2> We Love Animals!!</h2>
+                </div>
+                <h2>Adopt don't shop</h2>
+            </div>
+        );
+    }
 }
 export default Welcome;
