23cd6f2e114aeddc23eb65831cb5dc34845c376d
Kendra Kyndberg <kendrak922@gmail.com>
2020-06-26 13:03:07 -0500

    new font

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -1,20 +1,37 @@
 import './welcome.css';
 import React, { Component } from "react";
 import Mac from './Mac.JPG';
+import Circe from './Circe.JPG'
 
 
 
 class Welcome extends Component {
     render() {
         return (
-            <div className="container">
-                <img src={Mac} className="tabby" alt="tabbycat" />
-                <div className="right">
-                    <h1>Clyde's Pet Rescue</h1>
-                    <p>Welcome to Clyde's Pet Rescue! Let us introduce you to your new best friend. We believe life is better spent with pets. </p>
+            <div className="flex-container">
+                {/* <h1 className="title">Animal Rescue</h1> */}
+                <img src={Mac} className="tabby welcome-item" alt="Tabby cat" />
+                <h1 className="title">Animal Rescue</h1>
+                <img src={Circe} className="calico welcome-item" alt="Calico Cat" />
+                <div className="text-right welcome-item">
+                    <h1>Animal Rescue</h1>
+                    <p>Welcome to Pet Rescue! Let us introduce you to your new best friend. We believe life is better spent with pets. </p>
                     <h2> We Love Animals!!</h2>
                 </div>
-                <h2>Adopt don't shop</h2>
+                <span className="steps">
+                    <div className="stepOne process">
+                        <p className="number">1</p>
+                        <p>Head over to our "Adoptable Animal's" page to see if there's a pet that you think could be a good fit. </p>
+                    </div>
+                    <div className="stepTwo process">
+                        <p className="number">2</p>
+                        <p>Contact us and we will connect you with the foster to set up a Meet-and-Greet.</p>
+                    </div>
+                    <div className="stepThree process">
+                        <p className="number">3</p>
+                        <p>Once you have fallen in love just fill out an adoption form. We will reach out to you once you've been approved to let you know when you can take home your new family member.</p>
+                    </div>
+                </span>
             </div>
         );
     }
